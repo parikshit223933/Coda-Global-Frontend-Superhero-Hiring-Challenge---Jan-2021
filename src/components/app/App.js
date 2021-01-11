da6d8fb4a78098eb91f783eb2a71d5c72c999098
parikshit223933 <pk223933@gmail.com>
2021-01-11 07:42:41 +0530

    done with routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,23 @@
+import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Better from '../MockupPage1/Better'
 import Results from '../MockupPage2/Results'
 
-function App() {
-    return (
-        <div className="App">
-            <Results />
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route exact path="/" component={Better} />
+                        <Route exact path="/results" component={Results} />
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App
